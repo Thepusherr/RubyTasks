392f4807c49ba822416853c6f3225a046af7fcba
ThePusher <spunkspunkik@gmail.com>
2022-02-21 10:44:52 +0200

    change matrix

--- matrix.rb
+++ matrix.rb
@@ -1,23 +1,18 @@
 class Matrix
   def initialize(size)
     @matrix_size = size
+    return unless size > 1
     @array = draw_matrix(convert_to_identity(create_matrix(@matrix_size)))
   end
 
   private
 
   def create_matrix(size)
-    return unless size > 1
     Array.new(size) { Array.new(size, 0) }
   end
-
+  
   def convert_to_identity(array)
-    array.each.with_index do |row, i|
-      row.each.with_index do |_, j|
-        if i == j; array[i][j] = 1
-        end
-      end
-    end
+    array.each.with_index.map { |x,i| x.each.with_index.map { |_,j| (i == j) ? array[i][j] = 1 : array[i][j] = 0}}
   end
 
   def draw_matrix(array = [][])
